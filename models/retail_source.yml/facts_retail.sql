14e2f1a02df64d6aa6e6c7a7d1977af8a07fa69a
vinay reddy <vinayreddykovuri@gmail.com>
2026-03-04 11:34:03 +0000

    o

--- models/retail_source.yml/facts_retail.sql
+++ models/retail_source.yml/facts_retail.sql
@@ -1,14 +1,29 @@
-{{config(materialized='table')}}
+{{ config(
+    materialized='incremental',
+    unique_key='order_id'
+) }}
+
 select
-customer_sk,
-product_sk,
-order_id,
-order_date,
-o.customer_id,
-o.product_id,
-quantity,
-sales,
-profit
-from {{ref("stg_orders")}} o 
-left join {{ref("dim_cust")}} c on o.customer_id = c.customer_id
-left join {{ref("dim_products1")}} p on o.product_id = p.product_id
+o.order_id,
+o.order_date,
+c.customer_sk,
+p.product_sk,
+o.quantity,
+o.sales,
+o.profit
+
+from {{ ref('stg_orders') }} o
+
+left join {{ ref('dim_cust') }} c
+    on o.customer_id = c.customer_id
+
+left join {{ ref('dim_products1') }} p
+    on o.product_id = p.product_id
+
+{% if is_incremental() %}
+
+where o.order_date > (
+    select max(order_date) from {{ this }}
+)
+
+{% endif %}
