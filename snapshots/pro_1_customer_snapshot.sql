1891cce08bb1056d67349c4a47413d59aebc191f
vinay reddy <vinayreddykovuri@gmail.com>
2026-03-17 07:34:30 +0000

    updated

---
+++ snapshots/pro_1_customer_snapshot.sql
@@ -0,0 +1,18 @@
+{% snapshot pro1_customer_raw %}
+
+{{ config(
+    target_schema = 'public',
+    unique_key = 'customer_id',
+    strategy = 'check',
+    check_cols = ['address','email']
+)}}
+
+select 
+customer_id,
+name,
+email,
+address,
+updated_at
+from {{ source("pract_1","pro_1_customer_raw")}}
+
+{% endsnapshot %}
