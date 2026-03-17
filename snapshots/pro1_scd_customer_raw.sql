7c0cdc3ef6fc453e7bb0656bfc66392ce9080161
vinay reddy <vinayreddykovuri@gmail.com>
2026-03-17 08:10:24 +0000

    updated

---
+++ snapshots/pro1_scd_customer_raw.sql
@@ -0,0 +1,19 @@
+{% snapshot customer_raw_scd %}
+
+{{ config=(
+    target_schema = 'public',
+    unique_key = 'customer_id',
+    strategy = 'checks',
+    check_cols = ['email','address']
+
+)}}
+
+select 
+customer_id,
+name,
+email,
+address,
+updated_at
+from {{source("pract_1","CUSTOMER_RAW_SCD")}}
+
+{% endsnapshot%}
